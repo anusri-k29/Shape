1789d2eb2b031e932f6995fb6a39942dafe1d3e4
Anusri Kadam <anusrikadam@gmail.com>
2025-03-02 13:10:55 +0530

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -1,19 +1,23 @@
 // 3d shapes
 //Pyramid.java
-
-import java.lang.*;
-// Pyramid.java
 class Pyramid extends Shape {
     public Pyramid(double baseLength) {
         super(baseLength, 0, 0, 5);
     }
+
+    public double calculateArea() {
+        double baseArea = dim_one * dim_one;
+        double triangleArea = (Math.sqrt(3) / 4) * (dim_one * dim_one);
+        return baseArea + (4 * triangleArea); // Surface Area
+    }
+
     public double calculateVolume() {
         double height = (Math.sqrt(2.0 / 3.0) * dim_one);
         return (1.0 / 3) * (dim_one * dim_one) * height;
     }
-    public double calculateSurfaceArea() {
-        double baseArea = dim_one * dim_one;
-        double triangleArea = (Math.sqrt(3) / 4) * (dim_one * dim_one);
-        return baseArea + (4 * triangleArea);
+
+    public double calculatePerimeter() {
+        return 4 * dim_one; // Perimeter of the square base
     }
 }
+

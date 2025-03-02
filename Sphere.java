08c6e999a138d4111700f07b93cc7c48afa2ce43
Anusri Kadam <anusrikadam@gmail.com>
2025-03-02 13:10:18 +0530

    Update Sphere.java

--- Sphere.java
+++ Sphere.java
@@ -1,12 +1,19 @@
-// Sphere.java
-class Sphere extends Shape implements Volume {
+//Sphere.java
+class Sphere extends Shape {
     public Sphere(double radius) {
         super(radius, 0, 0, 0);
     }
+
+    public double calculateArea() {
+        return 4 * Math.PI * dim_one * dim_one; // Surface Area
+    }
+
     public double calculateVolume() {
         return (4.0 / 3.0) * Math.PI * Math.pow(dim_one, 3);
     }
-    public double calculateSurfaceArea() {
-        return 4 * Math.PI * dim_one * dim_one;
+
+    public double calculatePerimeter() {
+        return 0; // Not applicable for a sphere
     }
 }
+

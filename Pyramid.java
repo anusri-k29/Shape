94e8d9a245e9f78b8cdbed7fe7191342dc421640
Anusri Kadam <anusrikadam@gmail.com>
2025-03-02 12:55:02 +0530

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -2,20 +2,18 @@
 //Pyramid.java
 
 import java.lang.*;
-class Pyramid extends Shape{
-	
-	public Pyramid(double pyramidLength) {
-		super(pyramidLength,0,0,5);
-	}
-	public double calculateSurfaceArea() {
-		double baseArea = dim_one * dim_one; // Square base area
-        double triangleArea = (Math.sqrt(3) / 4) * (dim_one * dim_one); // Area of one equilateral triangle
+// Pyramid.java
+class Pyramid extends Shape {
+    public Pyramid(double baseLength) {
+        super(baseLength, 0, 0, 5);
+    }
+    public double calculateVolume() {
+        double height = (Math.sqrt(2.0 / 3.0) * dim_one);
+        return (1.0 / 3) * (dim_one * dim_one) * height;
+    }
+    public double calculateSurfaceArea() {
+        double baseArea = dim_one * dim_one;
+        double triangleArea = (Math.sqrt(3) / 4) * (dim_one * dim_one);
         return baseArea + (4 * triangleArea);
-
-	}
-	
-	public double calculateVolume(){
-		double height = (Math.sqrt(2.0 / 3.0) * dim_one); 
-		return (1.0 / 3) * (dim_one * dim_one) * height;
-	}
+    }
 }

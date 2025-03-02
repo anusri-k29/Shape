31fc5faef8e5043b86a98b3c2cda7f4ad04937b2
Anusri Kadam <anusrikadam@gmail.com>
2025-03-02 13:10:00 +0530

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,35 +1,27 @@
-//Shape.java
+abstract class Shape {
+    protected double dim_one;
+    protected double dim_two;
+    protected double dim_three;
+    protected int num_sides;
 
-abstract class Shape{
-	protected double dim_one;
-	protected double dim_two;
-	protected double dim_three;
-	protected int num_sides;
-	
-	//zero parameter constructor for prevention of edge case error
-	
-	public Shape() {
-		dim_one = dim_two = dim_three = 0; 
-		num_sides = 0;
-	}
-	//Parameterized constructor for user input
-	public Shape(double dim_one,double dim_two,double dim_three,int num_sides){
-		//taken from the user as an argument
-		this.dim_one = dim_one;
-		this.dim_two = dim_two;
-		this.dim_three = dim_three;
-		this.num_sides = num_sides;
-		
-	}
-	
-	abstract public double calculateArea();
-	abstract public double calculatePerimeter();
-	public double calculateVolume(); return 0; } 
-    	public double calculateSurfaceArea() { return 0; } 
-	
-	
-	public int getNumSides(){
-		return num_sides;
-	}
+    public Shape() {
+        dim_one = dim_two = dim_three = 0;
+        num_sides = 0;
+    }
 
+    public Shape(double dim_one, double dim_two, double dim_three, int num_sides) {
+        this.dim_one = dim_one;
+        this.dim_two = dim_two;
+        this.dim_three = dim_three;
+        this.num_sides = num_sides;
+    }
+
+    abstract public double calculateArea();
+    abstract public double calculatePerimeter();
+    
+    public double calculateVolume() { return 0; }
+    
+    public int getNumSides() {
+        return num_sides;
+    }
 }

1bcedc59345233753c5fa63b28ef92af25b84e64
Anusri Kadam <anusrikadam@gmail.com>
2025-03-02 13:10:38 +0530

    Update Cylinder.java

--- Cylinder.java
+++ Cylinder.java
@@ -1,12 +1,18 @@
-// Cylinder.java
-class Cylinder extends Shape implements Volume {
+//Cylinder.java
+class Cylinder extends Shape {
     public Cylinder(double radius, double height) {
         super(radius, height, 0, 0);
     }
+
+    public double calculateArea() {
+        return 2 * Math.PI * dim_one * (dim_one + dim_two); // Surface Area
+    }
+
     public double calculateVolume() {
         return Math.PI * dim_one * dim_one * dim_two;
     }
-    public double calculateSurfaceArea() {
-        return 2 * Math.PI * dim_one * (dim_one + dim_two);
+
+    public double calculatePerimeter() {
+        return 0; // Not applicable for a cylinder
     }
 }

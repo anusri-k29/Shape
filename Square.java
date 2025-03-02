53aa18890ec55ab5dfe80193d0d8365b2915893e
Anusri Kadam <anusrikadam@gmail.com>
2025-03-02 12:48:05 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,11 @@
+class Square extends Shape {
+    public Square(double side) {
+        super(side, 0, 0, 4);
+    }
+    public double calculateArea() {
+        return dim_one * dim_one;
+    }
+    public double calculatePerimeter() {
+        return 4 * dim_one;
+    }
+}
